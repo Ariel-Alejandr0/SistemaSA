5ba23745e69c2d1665a12d8f6bf59a052fce24aa
Vitor Rodrigues <129552443+rodrigvues@users.noreply.github.com>
2024-07-10 08:50:15 -0300

    explicando como usar HandleOnClick.jsx

--- frontSA/src/functions/HandleOnClick.jsx
+++ frontSA/src/functions/HandleOnClick.jsx
@@ -3,3 +3,13 @@ export function handleOnClick(e, link, navigate) {
     navigate(link)
 }
 
+/*  1. importe:
+        import { useNavigate } from 'react-router-dom'
+        import { handleOnClick } from '../../functions/HandleOnClick'
+        
+    2. antes dos html no return, e dentro da function da sua página, ponha:
+        const navigate = useNavigate()
+
+    3. e use esta chamada de função para usá-lo :
+        onClick={(e) => handleOnClick(e, '/qualidade/quantidade-erros', navigate)}
+*/
