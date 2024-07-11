d4562bd51122b065b8e418e77cc6401ee7641f87
Vitor Rodrigues <vitor_rodrigues3@estudante.sesisenai.org.br>
2024-07-11 08:58:26 -0300

    começando rastreio

--- frontSA/src/pages/rastreioRFID/ServicosRastreamento.jsx
+++ frontSA/src/pages/rastreioRFID/ServicosRastreamento.jsx
@@ -1,11 +1,28 @@
 import React from 'react'
-import { Outlet } from 'react-router-dom'
+import iconlogo from '../../assets/rastreio/images/iconlogo.png'
+import iconlocalizacao from '../../assets/rastreio/images/iconlocalização.png'
+import iconenvio from '../../assets/rastreio/images/icon_envio.png'
+import { useNavigate } from 'react-router-dom'
+import { handleOnClick } from '../../functions/HandleOnClick'
 
 export default function ServicosRastreamento() {
+  const navigate = useNavigate()
+
   return (
     <div>
-        Servicos Rastreamento
-        <Outlet/>
+      <div class="container">
+        <img class="iconlogo" src={iconlogo} alt="Icon" />
+        <div onClick={(e) => handleOnClick(e, '/qualidade/quantidade-erros', navigate)} class="button">
+            <img src={iconlocalizacao} alt="Track Icon" />
+            Quero Rastrear Minha Encomenda
+            <img src={iconlocalizacao} alt="Track Icon" />
+        </div>
+        <div onClick={(e) => handleOnClick(e, '/qualidade/quantidade-erros', navigate)} class="button">
+            <img src={iconenvio} alt="Send Icon" />
+            Quero Enviar
+            <img src={iconenvio} alt="Send Icon" />
+        </div>
+      </div>
     </div>
   )
 }
