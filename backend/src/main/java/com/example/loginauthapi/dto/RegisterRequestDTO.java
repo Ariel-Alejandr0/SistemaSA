15a87856f631dcea5f82a7e575c63493b84dd5b7
Renan Fiamoncini Rother <151479054+Renanfir@users.noreply.github.com>
2024-07-22 12:06:17 -0300

    Delete backend directory

--- backend/src/main/java/com/example/loginauthapi/dto/RegisterRequestDTO.java
+++
@@ -1,9 +0,0 @@
-package com.example.loginauthapi.dto;
-
-
-import java.time.Instant;
-import java.util.Date;
-
-public record RegisterRequestDTO(String nome, String email, String idPapel, String senha, Long idPessoa, Instant dataCadastro) {
-
-}
--- backend/src/main/java/com/example/loginauthapi/dto/ResponseDTO.java
+++
@@ -1,3 +0,0 @@
-package com.example.loginauthapi.dto;
-
-public record ResponseDTO (String nome,String idPapel, String token, Long id_usuario, Long id_pessoa) { }
