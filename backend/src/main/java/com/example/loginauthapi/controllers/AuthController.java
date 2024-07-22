42d37953368a1898c6422e43598cf65aa5ff1f7d
Renan Fiamoncini Rother <151479054+Renanfir@users.noreply.github.com>
2024-07-22 11:17:02 -0300

    Update AuthController.java

--- backend/src/main/java/com/example/loginauthapi/controllers/AuthController.java
+++ backend/src/main/java/com/example/loginauthapi/controllers/AuthController.java
@@ -1,64 +1,81 @@
 package com.example.loginauthapi.controllers;
 
-import com.example.loginauthapi.domain.user.User;
-import com.example.loginauthapi.dto.LoginRequestDTO;
-import com.example.loginauthapi.dto.RegisterRequestDTO;
-import com.example.loginauthapi.dto.ResponseDTO;
+import com.example.loginauthapi.domain.user.Machine;
+import com.example.loginauthapi.domain.user.Usuario;
+import com.example.loginauthapi.dto.machine.RegisterRequestDTOmachine;
+import com.example.loginauthapi.dto.machine.ResponseDTOmachine;
+import com.example.loginauthapi.dto.user.LoginRequestDTO;
+import com.example.loginauthapi.dto.user.RegisterRequestDTO;
+import com.example.loginauthapi.dto.user.ResponseDTO;
 import com.example.loginauthapi.infra.security.TokenService;
+import com.example.loginauthapi.repositories.MachineRepository;
 import com.example.loginauthapi.repositories.UserRepository;
+import com.example.loginauthapi.service.MachineService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.Instant;
+import java.util.List;
 import java.util.Optional;
 
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
 public class AuthController {
-    // Injeta automaticamente as dependências necessárias
     private final UserRepository repository;
+    private final MachineRepository machineRepository;
     private final PasswordEncoder passwordEncoder;
     private final TokenService tokenService;
+    private final MachineService machineService;
 
-    // Endpoint para registro de usuários
     @PostMapping("/register")
     public ResponseEntity<ResponseDTO> register(@RequestBody RegisterRequestDTO body) {
-        // Verifica se o email já está registrado
-        Optional<User> user = this.repository.findByEmail(body.email());
+        Optional<Usuario> user = this.repository.findByEmail(body.email());
 
-        // Se o usuário não existe, cria um novo usuário
         if (user.isEmpty()) {
-            User newUser = new User(body.name(), body.email(), body.idPapel(), passwordEncoder.encode(body.password()));
+            Usuario newUser = new Usuario(body.nome(), body.email(), body.idPapel(), passwordEncoder.encode(body.senha()), body.idPessoa(), body.dataCadastro());
             this.repository.save(newUser);
 
-            // Gera um token JWT para o novo usuário
             String token = this.tokenService.generateToken(newUser);
-            return ResponseEntity.ok(new ResponseDTO(newUser.getName(),newUser.getIdPapel(), token));
+            return ResponseEntity.ok(new ResponseDTO(newUser.getNome(), newUser.getIdPapel(), token, newUser.getId_usuario(), newUser.getIdPessoa()));
         }
-        // Se o usuário já existe, retorna uma resposta de erro
         return ResponseEntity.badRequest().build();
     }
 
-    // Endpoint para login de usuários
+    @PostMapping("/registerMachine")
+    public ResponseEntity<ResponseDTOmachine> registerMachine(@RequestBody RegisterRequestDTOmachine body) {
+        Optional<Machine> machine = machineRepository.findByName(body.name());
+
+        if (machine.isEmpty()) {
+            Optional<Usuario> usuario = repository.findById(String.valueOf(body.idUsuario()));
+            if (usuario.isPresent()) {
+                Machine newMachine = new Machine(body.name(), body.idTipoMaquina(), body.dataInstalacao(), body.setor(), usuario.get(), body.ultimaManutencao());
+                machineRepository.save(newMachine);
+                return ResponseEntity.ok(new ResponseDTOmachine(newMachine.getName(), newMachine.getSetor()));
+            } else {
+                return ResponseEntity.badRequest().build();
+            }
+        }
+        return ResponseEntity.badRequest().build();
+    }
+
     @PostMapping("/login")
     public ResponseEntity<ResponseDTO> login(@RequestBody LoginRequestDTO body) {
-        // Busca o usuário pelo email
-        User user = this.repository.findByEmail(body.email()).orElseThrow(() -> new RuntimeException("User not found"));
+        Usuario user = this.repository.findByEmail(body.email()).orElseThrow(() -> new RuntimeException("User not found"));
 
-        // Verifica se a senha fornecida corresponde à senha armazenada
-        if (passwordEncoder.matches(body.password(), user.getPassword())) {
-            // Gera um token JWT para o usuário autenticado
+        if (passwordEncoder.matches(body.senha(), user.getSenha())) {
             String token = this.tokenService.generateToken(user);
-            return ResponseEntity.ok(new ResponseDTO(user.getName(), user.getIdPapel() ,token));
-
+            return ResponseEntity.ok(new ResponseDTO(user.getNome(), user.getIdPapel(), token, user.getId_usuario(), user.getIdPessoa()));
         }
-        // Se a senha não corresponder, retorna uma resposta de erro
         return ResponseEntity.badRequest().build();
+    }
+
+    @GetMapping("/allMachines")
+    public ResponseEntity<List<Machine>> listMachines(){
+        var machines = machineService.listAllMachines();
+
+        return ResponseEntity.ok(machines);
     }
 }
