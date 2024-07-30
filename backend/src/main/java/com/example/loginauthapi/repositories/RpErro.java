a2ddb39cdd4e5b2a0f9ef3f6428fb778abe81da7
Kauã Mellato <126474688+kaua-S@users.noreply.github.com>
2024-07-30 08:36:54 -0300

    enviando as informações Repository

---
+++ backend/src/main/java/com/example/loginauthapi/repositories/RpErro.java
@@ -0,0 +1,7 @@
+package demo.sa304.Repository;
+
+import demo.sa304.Entity.Erro;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RpErro extends JpaRepository<Erro,Long> {
+}
