23e67da7834305304d074457a55c84cbcf25f961
Ariel-Alejandr0 <pessoal.ariel.alejandro@gmail.com>
2024-07-30 07:15:26 -0300

    fazendo o a tela de home para o monitaremento de máquinas.

---
+++ frontSA/src/components/monitoramentoMaquinas/GrafLinha.jsx
@@ -0,0 +1,70 @@
+import React from "react";
+import ReactApexChart from "react-apexcharts";
+
+class GrafLinha extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      series: [{
+        name: "OEE",
+        data: props.oees || []
+      }],
+      options: {
+        chart: {
+          height: 350,
+          type: 'line',
+          zoom: {
+            enabled: false
+          }
+        },
+        dataLabels: {
+          enabled: false
+        },
+        stroke: {
+          curve: 'straight'
+        },
+        title: {
+          text: 'Product Trends by Month',
+          align: 'left'
+        },
+        grid: {
+          row: {
+            colors: ['#f3f3f3', 'transparent'], // takes an array which will be repeated on columns
+            opacity: 0.5
+          }
+        },
+        xaxis: {
+          categories: props.dias || [],
+        }
+      }
+    };
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.oees !== this.props.oees || prevProps.dias !== this.props.dias) {
+      this.setState({
+        series: [{ name: "OEE", data: this.props.oees }],
+        options: {
+          ...this.state.options,
+          xaxis: {
+            categories: this.props.dias
+          }
+        }
+      });
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <div id="chart">
+          <ReactApexChart options={this.state.options} series={this.state.series} type="line" height={350} />
+        </div>
+        <div id="html-dist"></div>
+      </div>
+    );
+  }
+}
+
+export default GrafLinha;
--- frontSA/src/pages/monitoramentoMaquinas/HomeMM.jsx
+++ frontSA/src/pages/monitoramentoMaquinas/HomeMM.jsx
@@ -1,6 +1,9 @@
 import { useContext, useEffect, useState } from 'react'
 import { ServiceCtx } from '../../contexts/indusphere/Service'
 import * as StompJs from '@stomp/stompjs';
+import '../../styles/pages/monitoramentoMaquinas/home.css'
+import Gauge from '../../components/monitoramentoMaquinas/Gauge';
+import GrafLinha from '../../components/monitoramentoMaquinas/GrafLinha';
 
 
 export default function HomeMM() {
@@ -76,58 +79,71 @@ export default function HomeMM() {
     };
 
     return (
-        <div>
+        <div style={{
+            width: '100%',
+            maxHeight: '100%',
+            overflow: 'auto'
+        }}> 
             <div className="filter-container">
                 <select id="center">
-                    <option value="generalFactory">Centro de Trabalho: Geral Fábrica</option>
+                <option value="generalFactory">Centro de Trabalho: Geral Fábrica</option>
                 </select>
                 <select id="shift">
-                    <option value="firstShift">Turno: 1º turno</option>
+                <option value="firstShift">Turno: 1º turno</option>
                 </select>
                 <select id="period">
-                    <option value="currentWeek">Período: 01/02/2024 - 08/02/2024</option>
+                <option value="currentWeek">Período: 01/02/2024 - 08/02/2024</option>
                 </select>
             </div>
-            <div className="container">
+            <div >
                 <div className="dashboard">
-                    <h2>Dashboard Máquinas</h2>
-                    <p>Qtd. produzida / Qtd. planejada</p>
-                    <div className="progress">
-                        <div className="progress-bar" style={{ width: `${(contador / 4750) * 100}%` }} />
-                    </div>
-                    <p>{contador} / 4.750</p>
+                <h2>Dashboard Máquinas</h2>
+                <p>Qtd. produzida / Qtd. planejada</p>
+                <div className="progress">
+                    <div className="progress-bar" style={{width: '35%'}} />
+                </div>
+                <p>1.670 / 4.750</p>
+                </div>
+                <div className="metrics">
+                <div className="metric">
+                    <h3>OEE</h3>
+                    <Gauge num={3}/>
+                </div>
+                <div className="metric">
+                    <h3>Disponibilidade</h3>
+                    <Gauge num={25}/>
+                </div>
+                <div className="metric">
+                    <h3>Performance</h3>
+                    <Gauge num={100}/>
+                </div>
+                <div className="metric">
+                    <h3>Qualidade</h3>
+                    <Gauge num={80}/>
+                </div>
+                </div>
+                <div className="charts">
+                <div className="chart chart-small">
+                    <h3>OEE por Dia</h3>
+                    <GrafLinha oees={[10, 41, 35, 51, 49, 62, 69, 91, 100]} dias={['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep']}/>
+                </div>
+                <div className="chart chart-small">
+                    <h3>Timeline Máquinas</h3>
+                    <canvas id="lineChart" />
+                </div>
+                <div className="chart chart-small">
+                    <h3>Motivos de parada</h3>
+                    <canvas id="downtimeChart" />
+                </div>
                 </div>
             </div>
             <div id="notificationsSidebar" className="notifications-sidebar">
                 <h2>Notificações</h2>
                 <div className="notification">
-                    <p>Não há notificações no momento.</p>
+                <p>Não há notificações no momento.</p>
                 </div>
             </div>
-            <div>
-                <button id="connect" type="button" onClick={connect} disabled={connected}>
-                    Liga
-                </button>
-                <button id="disconnect" type="button" onClick={disconnect} disabled={!connected}>
-                    Desliga
-                </button>
-            </div>
-            <div id="conversation" style={{ display: connected ? 'block' : 'none' }}>
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Greeting</th>
-                        </tr>
-                    </thead>
-                    <tbody id="greetings">
-                        {greetings.map((greeting, index) => (
-                            <tr key={index}>
-                                <td>{greeting}</td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
-            </div>
         </div>
+
     )
 }
