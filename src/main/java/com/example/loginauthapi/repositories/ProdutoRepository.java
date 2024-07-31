6246c8605b84bd469e760acfdca43400df7b2d17
Renan Fiamoncini Rother <151479054+Renanfir@users.noreply.github.com>
2024-07-31 13:56:00 -0300

    Delete src directory

--- src/main/java/com/example/loginauthapi/repositories/ProdutoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.loginauthapi.repositories;
-
-
-import com.example.loginauthapi.domain.user.Produto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProdutoRepository extends JpaRepository<Produto, Long> {
-}
--- src/main/java/com/example/loginauthapi/repositories/RFIDRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.loginauthapi.repositories;
-
-import com.example.loginauthapi.domain.user.RFID;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RFIDRepository extends JpaRepository<RFID, Long> {
-}
